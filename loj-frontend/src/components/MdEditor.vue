41be1425be078ee923b4d8558b5259e4104b92a1
buglzl <2868177181@qq.com>
2024-07-15 17:13:57 +0800

    完成了markdown编辑器的组件

---
+++ loj-frontend/src/components/MdEditor.vue
@@ -0,0 +1,40 @@
+<template>
+  <Editor :value="value" :plugins="plugins" @change="handleChange" />
+</template>
+
+<script setup lang="ts">
+import gfm from "@bytemd/plugin-gfm";
+import highlight from "@bytemd/plugin-highlight";
+import math from "@bytemd/plugin-math";
+import { Editor, Viewer } from "@bytemd/vue-next";
+import { ref, withDefaults, defineProps } from "vue";
+import "katex/dist/katex.css";
+
+/**
+ * 定义组件属性类型
+ */
+interface Props {
+  value: string;
+  handleChange: (v: string) => void;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  value: () => "",
+  handleChange: (v: string) => {
+    console.log(v);
+  },
+});
+
+const plugins = [
+  gfm(),
+  highlight(),
+  math(),
+  // Add more plugins here
+];
+</script>
+
+<style>
+.bytemd-toolbar-icon.bytemd-tippy.bytemd-tippy-right:last-child {
+  display: none;
+}
+</style>
--- loj-frontend/src/main.ts
+++ loj-frontend/src/main.ts
@@ -5,5 +5,6 @@ import "@arco-design/web-vue/dist/arco.css";
 import router from "./router";
 import store from "./store";
 import "./access";
+import "bytemd/dist/index.css";
 
 createApp(App).use(store).use(ArcoVue).use(router).mount("#app");
